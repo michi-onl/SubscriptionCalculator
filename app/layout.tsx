88d81427586926d75bd1808a1e695f705ddd0bcd
Michael <87752300+michi-cc@users.noreply.github.com>
2025-06-03 22:57:24 +0200

    style: new font and auto dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Lora, Geist, Geist_Mono } from "next/font/google";
+import "@/app/globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+
+const loraSerif = Lora({
+  variable: "--font-lora-serif",
+  subsets: ["latin"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +29,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${loraSerif.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}{" "}
+        </ThemeProvider>
       </body>
     </html>
   );
